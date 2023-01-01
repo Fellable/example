ba0641e23e26987ae2cdecd02cf0023a02172cc0
Fellable <crash749@yandex.ru>
2023-01-01 17:26:08 +0300

    aaaaaaaaaaaa

---
+++ app/Http/Controllers/PostController333.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class PostController333 extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    $aaaaaaaaaaaa = 11111111111114341;
+}
